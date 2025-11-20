12144bebfe0feb647833f1939c9eb31d3770e948
Sakib Hadziavdic <sakib@sake.ba>
2025-11-20 13:56:38 +0100

    Add sources, resources, javacOptions, scalacOptions to Pkl definitions

--- scripts/gen-config-bindings.sh
+++ scripts/gen-config-bindings.sh
@@ -1,4 +1,12 @@
 
+
+rm -rf config/src
+rm -rf config/resources
+
 ./pkl-codegen-java config/DederProject.pkl -o config/src
 
+
 mv config/src/resources config/resources
+
+mv config/src/java/* config/src
+rmdir config/src/java
