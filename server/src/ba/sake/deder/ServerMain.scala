e1c0d7ee38e195e849ae40c475b056be4da27a24
Sakib Hadziavdic <sakib@sake.ba>
2025-12-01 16:35:35 +0100

    Add global server file lock to ensure only one server instance running

--- server/src/ba/sake/deder/ServerMain.scala
+++ server/src/ba/sake/deder/ServerMain.scala
@@ -1,14 +1,21 @@
 package ba.sake.deder
 
+import java.io.RandomAccessFile
+import java.nio.channels.FileLock
+import java.nio.channels.OverlappingFileLockException
 import java.util.concurrent.Executors
+import scala.util.Properties
+import scala.util.Using
 
 @main def serverMain(projectRootDir: String = "."): Unit = {
 
-  if getMajorJavaVersion < 17 then
+  if !Properties.isJavaAtLeast(17) then
     throw DederException("Must run with Java 17+")
 
   val projectRoot = os.pwd / os.SubPath(projectRootDir)
   System.setProperty("DEDER_PROJECT_ROOT_DIR", projectRoot.toString)
+
+  acquireServerLock(projectRoot)
 
   val tasksExecutorService = Executors.newFixedThreadPool(10)
   val projectState = DederProjectState(tasksExecutorService)
@@ -22,15 +29,25 @@ import java.util.concurrent.Executors
   cliServerThread.join()
 }
 
-def getMajorJavaVersion: Int = {
-  // Java 9+ versions are just the major number (e.g., "17")
-  // Java 8- versions start with "1." (e.g., "1.8.0_202")
-  val parts = scala.util.Properties.javaVersion.split('.')
-  if (parts.length > 1 && parts(0) == "1") {
-    // For Java 8 and earlier (e.g., "1.8.x"), the major version is the second part ("8")
-    parts(1).toIntOption.getOrElse(0)
-  } else {
-    // For Java 9+ (e.g., "9", "11", "17.0.1"), the major version is the first part
-    parts(0).toIntOption.getOrElse(0)
-  }
+def acquireServerLock(projectRoot: os.Path): Unit = {
+  val serverLockFile = projectRoot / ".deder/server.lock"
+  
+  os.makeDir.all(serverLockFile / os.up)
+
+  val lockFileHandle = new RandomAccessFile(serverLockFile.toIO, "rw")
+  val fileLock = lockFileHandle.getChannel.tryLock()
+  if fileLock == null then
+    println("ERROR: Could not acquire server lock - another server instance is already running for this project")
+    lockFileHandle.close()
+    sys.exit(1)
+
+  Runtime.getRuntime.addShutdownHook(new Thread(() => {
+    try {
+      fileLock.release()
+      lockFileHandle.close()
+      os.remove.all(serverLockFile)
+    } catch {
+      case _: Exception => // Ignore errors during cleanup
+    }
+  }))
 }
