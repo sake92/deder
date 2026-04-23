357015fa57a1dbc2016a07b01fb127d3daa87eba
Sakib Hadziavdic <sakib@sake.ba>
2026-04-23 11:29:17 +0200

    test: adapt DependencyResolverCacheSuite to class-based resolver

--- server/test/src/ba/sake/deder/deps/DependencyResolverCacheSuite.scala
+++ server/test/src/ba/sake/deder/deps/DependencyResolverCacheSuite.scala
@@ -3,24 +3,18 @@ package ba.sake.deder.deps
 class DependencyResolverCacheSuite extends munit.FunSuite {
 
   test("depsCacheKey is stable for same deps in different order") {
-    // Access private method via reflection
-    val method = DependencyResolver.getClass.getDeclaredMethod(
-      "depsCacheKey",
-      classOf[Seq[coursierapi.Dependency]]
-    )
-    method.setAccessible(true)
-
     val dep1 = coursierapi.Dependency.of("org.scala-lang", "scala-library", "2.13.12")
     val dep2 = coursierapi.Dependency.of("com.lihaoyi", "os-lib_2.13", "0.9.1")
 
-    val key1 = method.invoke(DependencyResolver, Seq(dep1, dep2)).asInstanceOf[String]
-    val key2 = method.invoke(DependencyResolver, Seq(dep2, dep1)).asInstanceOf[String]
+    val key1 = DependencyResolver.depsCacheKey(Seq(dep1, dep2))
+    val key2 = DependencyResolver.depsCacheKey(Seq(dep2, dep1))
 
     assertEquals(key1, key2, "Cache key should be order-independent (sorted)")
   }
 
   test("fetchFiles returns empty for empty deps") {
-    val result = DependencyResolver.fetchFiles(Seq.empty)
+    val resolver = new DependencyResolver(Seq.empty)
+    val result = resolver.fetchFiles(Seq.empty)
     assertEquals(result, Seq.empty)
   }
 }
