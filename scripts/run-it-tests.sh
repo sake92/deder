a0eca89413b0407cb533693c741e0fd37e214727
Sakib Hadziavdic <sakib@sake.ba>
2026-04-24 10:04:27 +0200

    Execute it tests with deder

--- scripts/run-it-tests.sh
+++ scripts/run-it-tests.sh
@@ -3,26 +3,14 @@ rm -rf ./tmp
 
 ./scripts/gen-config-bindings.sh
 
-./mill -i server.assembly
-./mill -i client.assembly       
+deder exec -m server -m client -t assembly
 
-export DEDER_SERVER_PATH=$(realpath out/server/assembly.dest/out.jar)
-export DEDER_CLIENT_PATH=$(realpath out/client/assembly.dest/out.jar)
+export DEDER_SERVER_PATH=$(realpath .deder/out/server/assembly/out.jar)
+export DEDER_CLIENT_PATH=$(realpath .deder/out/client/assembly/out.jar)
 
 if [ $# -eq 0 ]; then
-    ./mill -i integration.test
+    deder exec -m integration-test -t test
 else
-    ./mill -i integration.test.testOnly $1
+    deder exec -m integration-test -t test $1
 fi
-
-# TODO
-#deder exec -m server -t assembly
-#deder exec -m client -t assembly
-#export DEDER_SERVER_PATH=$(realpath .deder/out/server/assembly/out.jar)
-#export DEDER_CLIENT_PATH=$(realpath .deder/out/client/assembly/out.jar)
-#if [ $# -eq 0 ]; then
-#    deder exec -m integration-test -t test
-#else
-#    deder exec -m integration-test -t test $1
-#fi
 
