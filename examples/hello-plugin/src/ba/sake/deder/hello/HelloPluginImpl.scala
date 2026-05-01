1711235f07f89b09ffe396a0ab5406a6133b2990
Sakib Hadziavdic <sakib@sake.ba>
2026-05-01 08:54:56 +0200

    feat: add hello-plugin example project

---
+++ examples/hello-plugin/src/ba/sake/deder/hello/HelloPluginImpl.scala
@@ -0,0 +1,22 @@
+package ba.sake.deder.hello
+
+import ba.sake.deder.*
+import ba.sake.deder.config.DederProject.Plugin
+
+class HelloPluginImpl extends DederPlugin {
+  def id: String = "hello"
+
+  def tasks(coreTasks: CoreTasksApi, config: Plugin): Seq[AbstractTask[?]] = {
+    // Downcast to typed config
+    val helloConfig = config.asInstanceOf[HelloPlugin]
+    val greeting = Option(helloConfig.getGreeting()).getOrElse("Hello!")
+
+    val helloTask = TaskBuilder.make[String](name = "hello")
+      .build { ctx =>
+        ctx.notifications.add(ServerNotification.logInfo(greeting))
+        greeting
+      }
+
+    Seq(helloTask)
+  }
+}
