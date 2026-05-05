c88f5c25f27f97fa4a98e2fdf15fd52fcb227c6c
Sakib Hadziavdic <sakib@sake.ba>
2026-05-05 16:35:26 +0200

    feat: add SbtProjectAnalyzer replacing DederSbtExporter analysis logic

---
+++ server/src/ba/sake/deder/importing/sbt/SbtProjectAnalyzer.scala
@@ -0,0 +1,375 @@
+package ba.sake.deder.importing.sbt
+
+import ba.sake.deder.ServerNotification
+import ba.sake.deder.ServerNotificationsLogger
+import ba.sake.deder.config.DederProject
+import ba.sake.deder.importing._
+
+class SbtProjectAnalyzer(
+    serverNotificationsLogger: ServerNotificationsLogger
+) {
+
+    import SbtProjectAnalyzer._
+
+    def analyze(
+        exportedSbtModules: IndexedSeq[ProjectExport]
+    ): DederBuild = {
+        serverNotificationsLogger.add(ServerNotification.logInfo(s"Discovered ${exportedSbtModules.length} modules"))
+
+        // ---- PASS 1: group modules by cross-project root ----
+
+        val grouped = exportedSbtModules.groupBy { pe =>
+            val absPath = os.Path(pe.base)
+            val last = absPath.last
+            if (Set("jvm", "js", "native", ".jvm", ".js", ".native").contains(last)) absPath / os.up
+            else absPath
+        }.map { case (rootPath, modules) => RawGroup(rootPath, modules) }.toSeq
+
+        // Build group infos and global id mapping
+        val groupInfos = grouped.map(buildGroupInfo)
+        val globalIdMap: Map[String, String] = groupInfos.flatMap(_.sbtIdToRef).toMap
+
+        serverNotificationsLogger.add(ServerNotification.logInfo(
+            s"Resolved ${groupInfos.size} module groups, ${globalIdMap.size} id mappings"
+        ))
+
+        // ---- Topological sort ----
+        val sortedGroups = topoSort(groupInfos, globalIdMap)
+        serverNotificationsLogger.add(ServerNotification.logInfo(
+            s"Sorted order: ${sortedGroups.map(_.builderVarName).mkString(", ")}"
+        ))
+
+        // ---- Build ModuleDefs for each group ----
+        val warnings = Seq.newBuilder[ImportWarning]
+
+        val moduleGroups = sortedGroups.map { gi =>
+            val jvmModule = buildModuleDef(gi.mainModule, globalIdMap, warnings)
+            val jsModule = gi.allModules.find(pe => lastIsPlatform(os.Path(pe.base).last, "js"))
+                .map(pe => buildModuleDef(pe, globalIdMap, warnings, isJs = true))
+            val nativeModule = gi.allModules.find(pe => lastIsPlatform(os.Path(pe.base).last, "native"))
+                .map(pe => buildModuleDef(pe, globalIdMap, warnings, isNative = true))
+
+            ModuleGroup(
+                builderVarName = gi.builderVarName,
+                root = gi.root,
+                layout = gi.layout,
+                jvmModule = jvmModule,
+                jsModule = jsModule,
+                nativeModule = nativeModule,
+                hasJsModule = gi.hasJsModule,
+                hasNativeModule = gi.hasNativeModule,
+            )
+        }
+
+        _cachedSummary = buildSummary(moduleGroups, warnings.result())
+
+        DederBuild(
+            dederVersion = DederVersion,
+            moduleGroups = moduleGroups,
+            repositories = exportedSbtModules.flatMap(_.repositories).distinct.map(RepositoryDef.apply),
+            warnings = warnings.result(),
+        )
+    }
+
+    def summary(): ImportSummary = _cachedSummary
+
+    // ---- private state ----
+
+    private var _cachedSummary: ImportSummary = ImportSummary(0, 0, 0, 0, 0, Seq.empty, Seq.empty)
+
+    // ---- internal types ----
+
+    private case class RawGroup(rootPath: os.Path, modules: Seq[ProjectExport])
+
+    private case class GroupInfo(
+        builderVarName: String,
+        root: String,
+        layout: DederProject.DirLayout,
+        isCross: Boolean,
+        hasScalaJs: Boolean,
+        hasScalaNative: Boolean,
+        hasJsModule: Boolean,
+        hasNativeModule: Boolean,
+        scalaVersion: String,
+        sbtIdToRef: Map[String, String],
+        mainModule: ProjectExport,
+        allModules: Seq[ProjectExport],
+    )
+
+    private def buildGroupInfo(rg: RawGroup): GroupInfo = {
+        val root = rg.rootPath.relativeTo(os.pwd).toString match {
+            case "" => "."
+            case r  => r
+        }
+
+        val mainModule = rg.modules.find(pe =>
+            !pe.id.endsWith("Test") && !pe.id.contains("JS") && !pe.id.contains("Native")
+        ).getOrElse(rg.modules.head)
+
+        val allPlugins = rg.modules.flatMap(_.plugins).distinct
+        val layout = SbtProjectAnalyzer.detectLayout(allPlugins, rg.rootPath.toString)
+        val isCross = layout == DederProject.DirLayout.SBT_CROSS_FULL ||
+            layout == DederProject.DirLayout.SBT_CROSS_PURE ||
+            layout == DederProject.DirLayout.SBT_CROSS_DUMMY
+
+        val hasScalaJs = allPlugins.exists(p => p.contains("ScalaJSPlugin") || p.contains("scalajs"))
+        val hasScalaNative = allPlugins.exists(p => p.contains("ScalaNativePlugin") || p.contains("scalanative"))
+        val scalaVersion = mainModule.scalaVersion
+
+        val rawName = ImportingUtils.sanitizeId(mainModule.name)
+        val baseName = if (isCross) rawName.replaceFirst("(?i)jvm$", "") else rawName
+        val builderVarName = baseName.replaceAll("[.-]", "_").replaceAll("[^a-zA-Z0-9_]", "")
+
+        val sbtIdToRef = rg.modules.map { pe =>
+            val sbtId = pe.id
+            val rawSegment = os.Path(pe.base).last
+            val lastSegment = if (rawSegment.startsWith(".")) rawSegment.tail else rawSegment
+            val ref = if (isCross) s"$builderVarName.$lastSegment" else s"$builderVarName.main"
+            sbtId -> ref
+        }.toMap
+
+        val hasJsModule = rg.modules.exists(pe => Set("js", ".js").contains(os.Path(pe.base).last))
+        val hasNativeModule = rg.modules.exists(pe => Set("native", ".native").contains(os.Path(pe.base).last))
+
+        GroupInfo(
+            builderVarName, root, layout, isCross,
+            hasScalaJs, hasScalaNative, hasJsModule, hasNativeModule,
+            scalaVersion, sbtIdToRef, mainModule, rg.modules,
+        )
+    }
+
+    // ---- ModuleDef construction ----
+
+    private def buildModuleDef(
+        pe: ProjectExport,
+        idMap: Map[String, String],
+        warnings: scala.collection.mutable.Builder[ImportWarning, Seq[ImportWarning]],
+        isJs: Boolean = false,
+        isNative: Boolean = false,
+    ): ModuleDef = {
+        val (compileDeps, pluginDeps, testDeps) = partitionDeps(pe)
+        val moduleDeps = pe.interProjectDependencies
+            .filter(ipde => ipde.configuration == "default" || ipde.configuration.contains("compile"))
+            .flatMap(ipde => idMap.get(ipde.project))
+            .map(ref => refToModuleDepRef(ref, isTest = false))
+        val testModuleDeps = pe.interProjectDependencies
+            .filter(ipde => ipde.configuration.contains("test"))
+            .flatMap(ipde => idMap.get(ipde.project))
+            .map(ref => refToModuleDepRef(ref, isTest = true))
+            .distinct
+
+        // Publish info
+        val publish = if (pe.organization.nonEmpty || pe.version.nonEmpty) {
+            Some(PublishInfo(
+                organization = pe.organization,
+                artifactName = pe.artifactName,
+                version = pe.version,
+                description = if (pe.description.nonEmpty) Some(pe.description) else None,
+                homepage = pe.homepage,
+                developers = pe.developers.map(d => DeveloperDef(d.id, d.name, d.email)),
+                licenses = pe.licenses.map(l => LicenseDef(l.name, l.url)),
+                scmInfo = pe.scmInfo.map(s => ScmDef(s.browseUrl, s.connection, s.devConnection)),
+            ))
+        } else None
+
+        // Warnings
+        if (pe.crossScalaVersions.nonEmpty) {
+            warnings += ImportWarning.CrossScalaVersionsNotSupported(
+                moduleName = pe.name,
+                versions = pe.crossScalaVersions,
+                selectedVersion = pe.scalaVersion,
+            )
+        }
+
+        ModuleDef(
+            scalaVersion = pe.scalaVersion,
+            scalacOptions = pe.scalacOptions,
+            javacOptions = pe.javacOptions,
+            crossScalaVersions = pe.crossScalaVersions,
+            deps = compileDeps,
+            scalacPluginDeps = pluginDeps,
+            testDeps = testDeps,
+            moduleDeps = moduleDeps,
+            testModuleDeps = testModuleDeps,
+            scalaJsVersion = if (isJs) Some("1.18.2") else None,
+            scalaNativeVersion = if (isNative) Some("0.5.10") else None,
+            publish = publish,
+            sources = pe.sourceDirs,
+            testSources = pe.testSourceDirs,
+            resources = pe.resourceDirs,
+            testResources = pe.testResourceDirs,
+        )
+    }
+
+    private def partitionDeps(pe: ProjectExport): (Seq[DepDef], Seq[DepDef], Seq[DepDef]) = {
+        val compileDeps = pe.externalDependencies
+            .filterNot(d => isIgnoredDep(d.organization, d.name))
+            .filterNot(d => d.configurations.exists(c => c.contains("plugin") || c.contains("test") || c == "provided"))
+            .map(toDepDef)
+            .distinct
+        val pluginDeps = pe.externalDependencies
+            .filter(SbtProjectAnalyzer.isPluginDependency)
+            .filterNot(d => isIgnoredDep(d.organization, d.name))
+            .map(toDepDef)
+            .distinct
+        val testDeps = pe.externalDependencies
+            .filterNot(d => isIgnoredDep(d.organization, d.name))
+            .filter(d => d.configurations.exists(_.contains("test")))
+            .filterNot(d => d.configurations.exists(_.contains("plugin")))
+            .map(toDepDef)
+            .distinct
+        (compileDeps, pluginDeps, testDeps)
+    }
+
+    private def toDepDef(d: DependencyExport): DepDef = DepDef(
+        formatted = SbtProjectAnalyzer.formatDependency(d),
+        organization = d.organization,
+        name = d.name,
+    )
+
+    private def refToModuleDepRef(ref: String, isTest: Boolean): ModuleDepRef = {
+        val dotIdx = ref.indexOf('.')
+        val (group, platform) = if (dotIdx >= 0) {
+            (ref.substring(0, dotIdx), ref.substring(dotIdx + 1))
+        } else {
+            (ref, "main")
+        }
+        ModuleDepRef(targetGroup = group, targetPlatform = platform, isTest = isTest)
+    }
+
+    // ---- Summary ----
+
+    private def buildSummary(
+        groups: Seq[ModuleGroup],
+        warnings: Seq[ImportWarning],
+    ): ImportSummary = {
+        val concreteModules = groups.size * 2 + groups.count(_.hasJsModule) * 2 + groups.count(_.hasNativeModule) * 2
+        val allDeps = groups.flatMap(g =>
+            g.jvmModule.deps ++ g.jvmModule.scalacPluginDeps ++ g.jvmModule.testDeps
+        )
+        ImportSummary(
+            modulesImported = concreteModules,
+            moduleGroups = groups.size,
+            dependenciesMapped = allDeps.size,
+            depsFiltered = 0,
+            depsSkipped = 0,
+            warnings = warnings,
+            errors = Seq.empty,
+        )
+    }
+
+    // ---- Topological sort ----
+
+    private def topoSort(groups: Seq[GroupInfo], idMap: Map[String, String]): Seq[GroupInfo] = {
+        val builderNames = groups.map(_.builderVarName).toSet
+        val nameToGroup = groups.map(g => g.builderVarName -> g).toMap
+
+        val depsOf: Map[String, Set[String]] = groups.map { gi =>
+            val deps = gi.mainModule.interProjectDependencies
+                .filter(_.configuration == "default")
+                .flatMap(ipde => idMap.get(ipde.project))
+                .map(ref => ref.takeWhile(_ != '.'))
+                .filter(builderNames.contains)
+            gi.builderVarName -> deps.toSet
+        }.toMap
+
+        val dependents: Map[String, Set[String]] = {
+            val m = scala.collection.mutable.Map.empty[String, Set[String]].withDefaultValue(Set.empty)
+            depsOf.foreach { case (dependent, deps) =>
+                deps.foreach(d => m(d) = m(d) + dependent)
+            }
+            m.toMap
+        }
+
+        val inDegree = scala.collection.mutable.Map.from(
+            groups.map(g => g.builderVarName -> depsOf(g.builderVarName).size)
+        )
+        val queue = scala.collection.mutable.Queue.from(
+            groups.filter(g => inDegree(g.builderVarName) == 0)
+        )
+        val sorted = scala.collection.mutable.ListBuffer.empty[GroupInfo]
+
+        while (queue.nonEmpty) {
+            val g = queue.dequeue()
+            sorted += g
+            for (dependent <- dependents.getOrElse(g.builderVarName, Set.empty)) {
+                inDegree(dependent) -= 1
+                if (inDegree(dependent) == 0) {
+                    queue.enqueue(nameToGroup(dependent))
+                }
+            }
+        }
+        sorted.toSeq
+    }
+}
+
+object SbtProjectAnalyzer {
+
+    val DederVersion = "v0.7.4"
+
+    private val IgnoredDeps = Set(
+        ("org.scala-lang", "scala3-library"),
+        ("org.scala-lang", "scala-library"),
+        ("org.scala-js", "scalajs-library"),
+        ("org.scala-js", "scalajs-scalalib"),
+        ("org.scala-js", "scalajs-test-bridge"),
+        ("org.scala-js", "scalajs-test-interface"),
+        ("org.scala-js", "scalajs-compiler"),
+        ("org.scala-native", "scala3lib"),
+        ("org.scala-native", "scalalib"),
+        ("org.scala-native", "javalib"),
+        ("org.scala-native", "nativelib"),
+        ("org.scala-native", "auxlib"),
+        ("org.scala-native", "clib"),
+        ("org.scala-native", "posixlib"),
+        ("org.scala-native", "test-interface"),
+        ("org.scala-native", "windowslib"),
+        ("org.scala-native", "nscplugin"),
+    )
+
+    def isIgnoredDep(org: String, name: String): Boolean =
+        IgnoredDeps.exists { case (ignoredOrg, ignoredName) =>
+            org == ignoredOrg && name.startsWith(ignoredName)
+        }
+
+    def lastIsPlatform(s: String, platform: String): Boolean =
+        s == platform || s == s".$platform"
+
+    def isPluginDependency(dep: DependencyExport): Boolean = {
+        dep.configurations.exists(_.contains("plugin"))
+    }
+
+    def formatDependency(dep: DependencyExport): String = {
+        val scalaColon = dep.crossVersion match {
+            case "full"   => ":::"
+            case "binary" => "::"
+            case _        => ":"
+        }
+        val platformColon = dep.platformOpt match {
+            case Some(_) => "::"
+            case None    => ":"
+        }
+        s"${dep.organization}$scalaColon${dep.name}$platformColon${dep.revision}"
+    }
+
+    def detectLayout(plugins: Seq[String], projectBaseDir: String): DederProject.DirLayout = {
+        val hasCrossProject = plugins.exists(p =>
+            p.contains("sbt-crossproject") || p.contains("ScalaJSCrossPlugin") || p.contains("ScalaNativeCrossPlugin")
+        )
+        val basePath = os.Path(projectBaseDir)
+        val hasSharedDir = os.exists(basePath / "shared")
+        val hasDotDirs = os.exists(basePath / ".jvm") || os.exists(basePath / ".js") || os.exists(basePath / ".native")
+        val hasTopLevelPlatformDirs = os.exists(basePath / "jvm") && os.exists(basePath / "js")
+
+        if (hasCrossProject) {
+            if (hasSharedDir) DederProject.DirLayout.SBT_CROSS_FULL
+            else if (hasDotDirs) DederProject.DirLayout.SBT_CROSS_PURE
+            else if (hasTopLevelPlatformDirs) DederProject.DirLayout.SBT_CROSS_DUMMY
+            else DederProject.DirLayout.SBT
+        } else if (hasDotDirs) {
+            DederProject.DirLayout.SBT_CROSS_PURE
+        } else {
+            DederProject.DirLayout.SBT
+        }
+    }
+}
