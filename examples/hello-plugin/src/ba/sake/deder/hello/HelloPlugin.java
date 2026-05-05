0769c0949707e59c7835c080bab5d97d3c683f88
Sakib Hadziavdic <sakib@sake.ba>
2026-05-05 19:11:12 +0200

    fix: update example hello-plugin to match current DederPlugin API
    - Add @Named constructor to HelloPlugin.java (same pattern as HelloConfig fix)
    - Update HelloPluginImpl.scala from old config: Plugin signature to
      current configText: String with proper Pkl deserialization
    - Remove unused dummy HelloPluginConfig case class
    - Remove unsafe asInstanceOf downcast

--- examples/hello-plugin/src/ba/sake/deder/hello/HelloPlugin.java
+++ examples/hello-plugin/src/ba/sake/deder/hello/HelloPlugin.java
@@ -1,6 +1,9 @@
 package ba.sake.deder.hello;
 
 import ba.sake.deder.config.DederProject;
+import java.util.Collections;
+import java.util.List;
+import org.pkl.config.java.mapper.Named;
 
 /**
  * Generated Java binding for HelloPlugin.pkl.
@@ -8,6 +11,19 @@ import ba.sake.deder.config.DederProject;
  */
 public class HelloPlugin extends DederProject.Plugin {
     public String greeting;
+
+    /** Constructor used by Pkl's ConfigEvaluator to map values via @Named parameters. */
+    public HelloPlugin(@Named("id") String id,
+                       @Named("deps") List<String> deps,
+                       @Named("greeting") String greeting) {
+        super(id, deps);
+        this.greeting = greeting;
+    }
+
+    /** No-arg constructor for Java compilation. */
+    public HelloPlugin() {
+        this("", Collections.emptyList(), "Hello!");
+    }
 
     public String getGreeting() {
         return greeting;
--- examples/hello-plugin/src/ba/sake/deder/hello/HelloPluginImpl.scala
+++ examples/hello-plugin/src/ba/sake/deder/hello/HelloPluginImpl.scala
@@ -1,15 +1,25 @@
 package ba.sake.deder.hello
 
+import scala.util.Using
+import org.pkl.config.java.ConfigEvaluator
+import org.pkl.core.ModuleSource
 import ba.sake.deder.*
-import ba.sake.deder.config.DederProject.Plugin
 
 class HelloPluginImpl extends DederPlugin {
   def id: String = "hello"
 
-  def tasks(coreTasks: CoreTasksApi, config: Plugin): Seq[AbstractTask[?]] = {
-    // Downcast to typed config
-    val helloConfig = config.asInstanceOf[HelloPlugin]
-    val greeting = Option(helloConfig.getGreeting()).getOrElse("Hello!")
+  def tasks(coreTasks: CoreTasksApi, configText: String): Seq[AbstractTask[?]] = {
+    val config = Using.resource(ConfigEvaluator.preconfigured) { evaluator =>
+      evaluator.evaluate(ModuleSource.text(s"""
+        output {
+          new {
+            $configText
+          }
+        }
+      """)).as(classOf[HelloPlugin])
+    }
+
+    val greeting = Option(config.getGreeting()).getOrElse("Hello!")
 
     val helloTask = TaskBuilder.make[String](name = "hello")
       .build { ctx =>
@@ -20,6 +30,3 @@ class HelloPluginImpl extends DederPlugin {
     Seq(helloTask)
   }
 }
-
-// Dummy class to ensure .tasty files are generated (required for scaladoc during publish)
-case class HelloPluginConfig(greeting: String = "Hello!")
