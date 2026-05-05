78f8222f20689ea6a0158ddbc84e349e48ac0b63
Sakib Hadziavdic <sakib@sake.ba>
2026-05-05 21:12:08 +0200

    fix: use Json.parseObject + containsKey for safe JSON access

--- examples/hello-plugin/src/ba/sake/deder/hello/HelloPluginImpl.scala
+++ examples/hello-plugin/src/ba/sake/deder/hello/HelloPluginImpl.scala
@@ -6,12 +6,8 @@ class HelloPluginImpl extends DederPlugin {
   def id: String = "hello"
 
   def tasks(coreTasks: CoreTasksApi, jsonConfig: String): Seq[AbstractTask[?]] = {
-    val jsonNode = org.pkl.core.util.json.Json.parse(jsonConfig)
-    val greeting = {
-      val obj = jsonNode.asObject()
-      if obj.containsKey("greeting") then obj.get("greeting").asString()
-      else "Hello!"
-    }
+    val jsonObj = org.pkl.core.util.json.Json.parseObject(jsonConfig)
+    val greeting = if jsonObj.containsKey("greeting") then jsonObj.getString("greeting") else "Hello!"
 
     val helloTask = TaskBuilder.make[String](name = "hello")
       .build { ctx =>
