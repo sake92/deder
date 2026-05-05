907bc312023d098289eb6e0ab03bb3df457a4272
Sakib Hadziavdic <sakib@sake.ba>
2026-05-05 15:44:41 +0200

    fix: wrap JSON in 'output { value = ... }' for Pkl to evaluate as typed HelloConfig

--- integration/test/resources/sample-projects/hello-plugin/src/ba/sake/deder/hello/HelloPluginImpl.scala
+++ integration/test/resources/sample-projects/hello-plugin/src/ba/sake/deder/hello/HelloPluginImpl.scala
@@ -2,26 +2,30 @@ package ba.sake.deder.hello
 
 import scala.util.Using
 import org.pkl.config.java.ConfigEvaluator
-import org.pkl.core.{ModuleSource, PklException}
+import org.pkl.core.ModuleSource
 import ba.sake.deder.*
 
 class HelloPluginImpl extends DederPlugin {
   def id: String = "hello"
 
-  def tasks(coreTasks: CoreTasksApi, pklConfigSource: String): Seq[AbstractTask[?]] = {
-    println(s"HelloPluginImpl: Parsing config: $pklConfigSource")
-    val parsedConfig1 = Using.resource(ConfigEvaluator.preconfigured) { configEvaluator =>
-      configEvaluator.evaluate(ModuleSource.text(pklConfigSource)).as(classOf[HelloConfig])
+  def tasks(coreTasks: CoreTasksApi, jsonConfig: String): Seq[AbstractTask[?]] = {
+    // Wrap JSON object as a Pkl expression value, then convert to typed HelloConfig
+    // (Pkl evaluator can't parse a JSON object as a module top-level, but it can as an expression)
+    val pklSource =
+      s"""|output {
+          |  value = $jsonConfig
+          |}
+          |""".stripMargin
+    val config = Using.resource(ConfigEvaluator.preconfigured) { configEvaluator =>
+      configEvaluator.evaluate(ModuleSource.text(pklSource)).as(classOf[HelloConfig])
     }
-    println(s"HelloPluginImpl: Got config: $parsedConfig1")
-    val greeting = parsedConfig1.getGreeting()
+    val greeting = config.getGreeting()
 
     val helloTask = TaskBuilder
       .make[String](name = "hello")
       .build { ctx =>
         ctx.notifications.add(ServerNotification.logInfo(greeting, ctx.module.id))
         greeting
-        "result..."
       }
     Seq(helloTask)
   }
