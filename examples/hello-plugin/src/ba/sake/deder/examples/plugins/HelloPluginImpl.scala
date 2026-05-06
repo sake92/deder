ab1621ce68478712ac0984e6d799e061857cd0c0
Sakib Hadziavdic <sakib@sake.ba>
2026-05-06 14:47:00 +0200

    Use modulepath in plugin

--- examples/hello-plugin/src/ba/sake/deder/examples/plugins/HelloPluginImpl.scala
+++ examples/hello-plugin/src/ba/sake/deder/examples/plugins/HelloPluginImpl.scala
@@ -10,7 +10,7 @@ class HelloPluginImpl extends DederPluginApi {
 
   def tasks(coreTasks: CoreTasksApi, pcfText: String): Seq[AbstractTask[?]] = {
     val moduleText =
-      s"""|amends "file:///home/sake/projects/sake92/deder/.worktrees/add-plugins-support/examples/hello-plugin/HelloPlugin.pkl"
+      s"""|amends "modulepath:/hello-plugin/HelloPlugin.pkl"
           |
           |$pcfText
           |
