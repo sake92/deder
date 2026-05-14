544c76f6b074db12f11e41cee5c68f46635c4735
Sakib Hadziavdic <sakib@sake.ba>
2026-05-14 15:08:25 +0200

    fix: update IntegrationSuite plan output expectation to match current task DAG

--- integration/test/src/ba/sake/deder/IntegrationSuite.scala
+++ integration/test/src/ba/sake/deder/IntegrationSuite.scala
@@ -62,7 +62,6 @@ class IntegrationSuite extends BaseIntegrationSuite {
         assertEquals(
           dederOutput,
           """|Stage #0:
-             |  common.allGeneratedSources
              |  common.classes
              |  common.compileOnlyDeps
              |  common.deps
@@ -79,19 +78,11 @@ class IntegrationSuite extends BaseIntegrationSuite {
              |  common.semanticdbEnabled
              |  common.sources
              |Stage #1:
-             |  common.allClassesDirs
-             |  common.allGeneratedSourceFiles
-             |  common.compileOnlyDependencies
              |  common.compilerDeps
-             |  common.dependencies
              |  common.javacAnnotationProcessors
-             |  common.mandatoryDependencies
              |  common.scalaSemanticdbVersion
              |  common.scalacPlugins
              |  common.sourceFiles
-             |Stage #2:
-             |  common.allDependencies
-             |  common.compilerJars
              |Stage #3:
              |  common.compileClasspath
              |Stage #4:
