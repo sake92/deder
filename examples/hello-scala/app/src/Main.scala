ab6ada86b97f9223b0daf3dfda62f142cc543490
Sakib Hadziavdic <sakib@sake.ba>
2026-07-07 16:02:02 +0200

    add examples/hello-scala

---
+++ examples/hello-scala/app/src/Main.scala
@@ -0,0 +1,8 @@
+// fdsfds 
+
+@main def app(): Unit = {
+  pprint.pprintln(calc(1, 5))
+}
+
+def calc(a: Int, b: Int): Int =
+  a + b
---
+++ examples/hello-scala/app/test/src/AppSuite.scala
@@ -0,0 +1,6 @@
+
+class AppSuite extends munit.FunSuite {
+  test("calc should calculate") {
+    assertEquals(calc(2, 2), 4)
+  }
+}
